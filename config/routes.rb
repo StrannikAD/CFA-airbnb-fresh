b669f91702032e5c3e80690c02e7cd358f809d3f
StrannikAD <a.dimitrova.au@gmail.com>
2017-04-04 02:20:58 +1000

    Updated functionality - added dashboards for hosts and guests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :bookings
   root 'pages#home'
-
+  get 'hostdashboard', to: 'pages#host_dashboard'
+get 'guestdashboard', to: 'pages#guest_dashboard'
   resources :rooms
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
